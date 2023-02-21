089ab93b86327dd1f17fec9d405cafbd4b4f9d34
thakshila rajapaksha <thakshila_rajapaksha@marubun.co.jp>
2023-02-21 18:10:22 +0900

    Add functionalities to the app
    Application header
    conditional formatting for the error message
    text box to input limit value
    page alignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-//import logo from "./logo.svg";
-import './App.css';
 import "@aws-amplify/ui-react/styles.css";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import {
   withAuthenticator,
   Button,
-  Heading,
-  //Image,
   View,
   Card,
+  TextField
 } from "@aws-amplify/ui-react";
 
 function App({ signOut }) {
+  // データベースの条件値の値を設定する
+  const isWithinLimit = '';
   return (
-    <View>
-      <Card>
-        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1} className="App-header">ネットワーク監視サービス</Heading>
-      </Card>
-      <Button onClick={signOut} className="App-button">Sign Out</Button>
-    </View>
+      <View>
+        {/* アプリケーションのヘッダー */}
+        <Box sx={{ flexGrow: 1 }}>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                    ネットワーク監視サービス
+                  </Typography>
+                  <Button color="inherit" onClick={signOut}>SignOut</Button>
+                </Toolbar>
+              </AppBar>
+        </Box>
+
+        {/* 条件値が超えている場合エラーメッセージが表示される */}
+        <Card>
+          <div>
+            {isWithinLimit ?
+              <Typography color="Blue">※ネットワークが正常通り。</Typography> :
+              <Typography color="Red">※ワーニングがあります。</Typography> 
+            }
+          </div>
+        </Card>
+        
+        <Card>
+            <TextField id="outlined-basic" label="設定値" variant="outlined" />
+        </Card>
+
+        {/* Grafanaの表示画面へ移動する */}    
+        <Card>
+          <Button variant="contained" onClick={(e) => {
+            window.open("http://13.113.48.181:3000/login", "_blank");
+            }} color="blue">
+              視覚化
+          </Button>
+        </Card>
+        
+      </View>
   );
 }
 
